84d7035fc3c9759ae46452a386f7d0c673177503
Victor Gallen <victorgallenrenau@gmail.com>
2022-01-16 13:46:38 +0100

    Create view to insipiration flight searcher

---
+++ src/app/inspiration-searcher/views/inspiration-searcher-view/inspiration-searcher-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mis-inspiration-searcher-view',
+  templateUrl: './inspiration-searcher-view.component.html',
+  styleUrls: ['./inspiration-searcher-view.component.scss']
+})
+export class InspirationSearcherViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
